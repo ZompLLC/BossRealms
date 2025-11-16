848f224ecabdd93ae8a963d03b0822a221e59f49
Keith Zhang <keithohno@gmail.com>
2025-11-16 18:40:51 -0500

    Added configuration for maven post-build

--- src/main/java/bossrealms/plugin/Main.java
+++
@@ -1,7 +0,0 @@
-package bossrealms.plugin;
-
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
-    }
-}
---
+++ src/main/java/bossrealms/plugin/Plugin.java
@@ -0,0 +1,10 @@
+package bossrealms.plugin;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Plugin extends JavaPlugin {
+    @Override
+    public void onEnable() {
+        System.out.println("HELLO WORLD");
+    }
+}
